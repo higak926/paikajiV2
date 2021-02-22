79fa96341f8150c8699355a299f4fc5d67856685
Higa Kazuaki <higak926@gmail.com>
2021-02-22 13:11:31 +0000

    home logo image fix etc

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -20,7 +20,11 @@ Rails.application.configure do
 
   # Disable serving static files from the `/public` folder by default since
   # Apache or NGINX already handles this.
-  config.public_file_server.enabled = true
+  config.public_file_server.enabled = ENV['RAILS_SERVE_STATIC_FILES'].present?
+
+  #config.public_file_server.headers = {
+    #'Cache-Control' => 'public, max-age=31536000'
+  #}
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor = :uglifier
@@ -31,12 +35,12 @@ Rails.application.configure do
   config.assets.initialize_on_precompile=true
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = '//s3.amazonaws.com/paikaji-bkt'
+  #config.action_controller.asset_host = '//s3.amazonaws.com/paikaji-bkt'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -4,4 +4,13 @@ AssetSync.configure do |config|
   config.aws_iam_roles = true
   config.fog_public = false
   config.fog_region = ENV['AWS_REGION']
+
+  config.gzip_compression      = true
+  config.manifest              = true
+
+  config.add_local_file_paths do
+    Dir.chdir(Rails.root.join('public')) do
+      Dir[File.join(Webpacker.config.public_output_path, '/**/**')]
+    end
+  end
 end
