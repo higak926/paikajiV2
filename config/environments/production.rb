22903a4f7aa247d7ba8148470780c334e90c17bc
Higa Kazuaki <higak926@gmail.com>
2021-02-23 05:24:33 +0000

    add CloudFront

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -40,7 +40,7 @@ Rails.application.configure do
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  #config.action_controller.asset_host = '//s3.amazonaws.com/paikaji-bkt'
+  config.action_controller.asset_host = ENV['AWS_CloudFront_DN'] 
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
