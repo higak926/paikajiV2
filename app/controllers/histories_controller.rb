e0ddfe95574ef22f1722bab56c98d3dbf1ad2a76
kazuakihiga <higak926@gmail.com>
2021-03-01 17:11:20 +0900

    update responsive

--- app/controllers/histories_controller.rb
+++ app/controllers/histories_controller.rb
@@ -1,5 +1,19 @@
 class HistoriesController < ApplicationController
+  before_action :detect_devise_variant
+
   def index
     @histories = History.all.order(:id)
+  end
+
+  private
+  def detect_devise_variant
+      case request.user_agent
+      when /iPad/
+          request.variant = :mobile
+      when /iPhone/
+          request.variant = :mobile
+      when /Android/
+          request.variant = :mobile
+      end
   end
 end
