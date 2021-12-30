f6242188074871e3df32f147bf3bde9718eb7a25
kazuakihiga <higak926@gmail.com>
2021-12-30 23:25:35 +0900

    remove assetasync

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -30,17 +30,17 @@ Rails.application.configure do
   config.assets.js_compressor = :uglifier
   config.assets.css_compressor = :sass
 
-  config.assets.enabled=true
-  config.assets.digest=true
-  config.assets.initialize_on_precompile=true
+  config.assets.enabled = false
+  config.assets.digest = false
+  config.assets.initialize_on_precompile = false
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = ENV['AWS_CloudFront_DN']
+  # config.action_controller.asset_host = ENV['AWS_CloudFront_DN']
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
--- config/initializers/asset_sync.rb
+++ config/initializers/asset_sync.rb
@@ -1,16 +1,16 @@
-AssetSync.configure do |config|
-  config.fog_provider = 'AWS'
-  config.fog_directory = ENV['AWS_BUCKET_NAME']
-  config.aws_iam_roles = true
-  config.fog_public = false
-  config.fog_region = ENV['AWS_REGION']
+# AssetSync.configure do |config|
+#   config.fog_provider = 'AWS'
+#   config.fog_directory = ENV['AWS_BUCKET_NAME']
+#   config.aws_iam_roles = true
+#   config.fog_public = false
+#   config.fog_region = ENV['AWS_REGION']
 
-  config.gzip_compression      = true
-  config.manifest              = true
+#   config.gzip_compression      = true
+#   config.manifest              = true
 
-  config.add_local_file_paths do
-    Dir.chdir(Rails.root.join('public')) do
-      Dir[File.join(Webpacker.config.public_output_path, '/**/**')]
-    end
-  end
-end
+#   config.add_local_file_paths do
+#     Dir.chdir(Rails.root.join('public')) do
+#       Dir[File.join(Webpacker.config.public_output_path, '/**/**')]
+#     end
+#   end
+# end
